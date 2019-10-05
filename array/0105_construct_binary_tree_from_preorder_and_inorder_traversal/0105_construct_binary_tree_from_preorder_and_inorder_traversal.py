373e67e0c081332446a1bc4fbf39e900511e777b
yiran <zdyxry@gmail.com>
2019-10-06 07:43:47 +0800

    update 0105

--- array/0105_construct_binary_tree_from_preorder_and_inorder_traversal/0105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ array/0105_construct_binary_tree_from_preorder_and_inorder_traversal/0105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -2,7 +2,7 @@ class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
-        self.right = None 
+        self.right = None
 
 
 class Solution(object):
@@ -10,20 +10,25 @@ class Solution(object):
         lookup = {}
         for i, num in enumerate(inorder):
             lookup[num] = i
-        return self.buildTreeRecu(lookup, preorder, inorder, 0, 0, len(inorder))
+        return self.buildTreeRecu(lookup, preorder, inorder, 0, 0,
+                                  len(inorder))
 
-    def buildTreeRecu(self, lookup, preorder, inorder, pre_start, in_start, in_end):
+    def buildTreeRecu(self, lookup, preorder, inorder, pre_start, in_start,
+                      in_end):
         if in_start == in_end:
-            return None 
+            return None
         node = TreeNode(preorder[pre_start])
         i = lookup[preorder[pre_start]]
-        node.left = self.buildTreeRecu(lookup, preorder, inorder, pre_start+1, in_start, i)
-        node.right = self.buildTreeRecu(lookup, preorder, inorder, pre_start +1 +i - in_start, i +1, in_end)
+        node.left = self.buildTreeRecu(lookup, preorder, inorder,
+                                       pre_start + 1, in_start, i)
+        node.right = self.buildTreeRecu(lookup, preorder, inorder,
+                                        pre_start + 1 + i - in_start, i + 1,
+                                        in_end)
         return node
 
 
-preorder = [3,9,20,15,7]
-inorder = [9,3,15,20,7]
+preorder = [3, 9, 20, 15, 7]
+inorder = [9, 3, 15, 20, 7]
 
 res = Solution().buildTree(preorder, inorder)
 print(res.val)
