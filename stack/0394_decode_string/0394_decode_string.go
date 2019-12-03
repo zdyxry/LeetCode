8c04a4d20f42dce6e2777e2ff7f9f753e3896ad4
yiran <zdyxry@gmail.com>
2019-12-03 08:11:03 +0800

    update 0394

--- stack/0394_decode_string/0394_decode_string.go
+++ stack/0394_decode_string/0394_decode_string.go
@@ -6,37 +6,54 @@ import (
 )
 
 func decodeString(s string) string {
-	stack , res := []string{}, ""
-	for _, str := range s {
-		if len(stack) == 0 || (len(stack) > 0 && str != ']') {
-			stack = append(stack, string(str))
-		} else {
-			tmp := ""
-			for stack[len(stack)-1] != "[" {
-				tmp = stack[len(stack)-1] + tmp
-				stack = stack[:len(stack)-1]
-			}
-			stack = stack[:len(stack)-1]
-			index, repeat := 0 , ""
-			for index = len(stack) - 1; index >= 0 ; index-- {
-				if stack[index] >= "0" && stack[index] <= "9" {
-					repeat = stack[index] + repeat
-				} else {
-					break
-				}
-			}
-			nums , _ := strconv.Atoi(repeat)
-			copyTmp := tmp
-			for i:=0;i< nums-1; i++ {
-				tmp += copyTmp
-			}
-			for i := 0 ; i< len(repeat)-1; i++ {
-				stack = stack[:len(stack)-1]
-			}
-			stack[index+1] = tmp
+    n := len(s)
+
+	// i 是第一个数字的位置
+	i := 0
+	for i < n && (s[i] < '0' || '9' < s[i]) {
+		i++
+	}
+	if i == n {
+		// 没有数字，直接返回 s
+		return s
+	}
+
+	// j 是第一个 '[' 的位置
+	j := i + 1
+	// 由题意可知，s 很规范
+	// 存在数字的话，必定存在 '[' 和 ']'
+	for s[j] != '[' {
+		j++
+	}
+
+	// k 是与 j 的 '[' 对应的 ']' 的位置
+	k := j
+	count := 1
+	for count > 0 {
+		k++
+
+		if s[k] == '[' {
+			count++
+		} else if s[k] == ']' {
+			count--
 		}
 	}
-	for _, s := range stack {
+
+	//      i：第一个数字的位置
+	//      |  j：第一个 '[' 的位置
+	//      |  |       k：与 j 的 '[' 对应的 ']' 的位置
+	//      ↓  ↓       ↓
+	// "abcd234[*******]efg"
+
+	// 题目说了， s 很规范
+	num, _ := strconv.Atoi(s[i:j])
+
+	return s[:i] + times(num, decodeString(s[j+1:k])) + decodeString(s[k+1:])
+}
+
+func times(n int, s string) string {
+	res := ""
+	for i := 0; i < n; i++ {
 		res += s
 	}
 	return res
