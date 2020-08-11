a28d937b96206e4fe2575b36f89de8123d0046b0
zdyxry <zdyxry@gmail.com>
2020-08-11 08:12:04 +0800

    update 1545

--- string/1545_find_kth_bit_in_nth_binary_string/1545_find_kth_bit_in_nth_binary_string.go
+++ string/1545_find_kth_bit_in_nth_binary_string/1545_find_kth_bit_in_nth_binary_string.go
@@ -1,12 +1,29 @@
-package main 
-import "fmt"
+package main
+
+import (
+	"fmt"
+	"math"
+)
 
 func findKthBit(n int, k int) byte {
+	if n == 1 {
+		return '0'
+	}
+	l := int(math.Pow(2, float64(n)) - 1)
+	mid := int(l/2 + 1)
+	if k == mid {
+		return '1'
+	} else if k < mid {
+		return findKthBit(n-1, k)
+	} else {
+		return byte(1 - int(findKthBit(n-1, int(l-k+1))))
+	}
+
 }
 
 func main() {
 	n := 3
 	k := 1
 	res := findKthBit(n, k)
-	fmt.Println(res)
+	fmt.Println(string(res))
 }
