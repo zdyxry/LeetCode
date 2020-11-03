35352f657ccb78bda0a0260dec280e6256dd6734
zdyxry <zdyxry@gmail.com>
2020-11-04 07:29:45 +0800

    update 0179

--- sort/0179_largest_number/0179_largest_number.py
+++ sort/0179_largest_number/0179_largest_number.py
@@ -1,10 +1,14 @@
+from typing import List
+class StrLt(str):
+    def __lt__(x, y):
+        return x+y > y+x
 
-class Solution(object):
-    def largestNumber(self, nums):
-        num = [str(x) for x in nums]
-        num.sort(cmp=lambda x, y: cmp(y + x, x + y))
-        largest = "".join(num)
-        return largest.lstrip("0") or "0"
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        strn = [str(n) for n in nums]
+        strn.sort(key=StrLt)
+        return ''.join(strn) if strn[0] != '0' else '0'
+
 
 nums = [10, 2]
 res = Solution().largestNumber(nums)
