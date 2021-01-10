bef5f8cba4984c9cfd990bcced38493e7db184cc
zdyxry <zdyxry@gmail.com>
2021-01-11 07:48:26 +0800

    update 1716

--- math/1716_calculate_money_in_leetcode_bank/1716_calculate_money_in_leetcode_bank.go
+++ math/1716_calculate_money_in_leetcode_bank/1716_calculate_money_in_leetcode_bank.go
@@ -5,10 +5,10 @@ import "fmt"
 func totalMoney(n int) int {
 
 	week, day := n/7, n%7
-	firstWeek := 28 // 7 * 8 / 2
-
+	firstWeek := 7 * 8 / 2
+	fmt.Println(week, day)
 	// n*a1 + d*n*(n-1)/2
-	return (week*firstWeek + 7*week*(week-1)/2) + week*day + day*(day+1)/2
+	return (week*firstWeek + 7*week*(week-1)/2) + (week+1)*day + day*(day-1)/2
 }
 
 func main() {
--- math/1716_calculate_money_in_leetcode_bank/1716_calculate_money_in_leetcode_bank.md
+++ math/1716_calculate_money_in_leetcode_bank/1716_calculate_money_in_leetcode_bank.md
@@ -51,9 +51,9 @@ func totalMoney(n int) int {
 
     week, day := n/7, n%7
     firstWeek := 7*8/2
-
+    fmt.Println(week, day)
     // n*a1 + d*n*(n-1)/2 
-    return (week * firstWeek + 7 * week*(week-1)/2) + week*day + day*(day+1)/2
+    return (week * firstWeek + 7 * week*(week-1)/2) + (week+1)*day + day*(day-1)/2
 }
 
 ```
