70e5d927aa4c02c256926b7896da7557203891c4
zdyxry <zdyxry@gmail.com>
2022-03-13 12:12:11 +0800

    update 0033

--- array/0033_search_in_rotated_sorted_array/0033_search_in_rotated_sorted_array.md
+++ array/0033_search_in_rotated_sorted_array/0033_search_in_rotated_sorted_array.md
@@ -95,3 +95,29 @@ class Solution(object):
         return -1
 
 ```
+
+
+```python
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[len(nums) - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
+```
--- array/0033_search_in_rotated_sorted_array/0033_search_in_rotated_sorted_array.py
+++ array/0033_search_in_rotated_sorted_array/0033_search_in_rotated_sorted_array.py
@@ -1,27 +1,25 @@
-class Solution(object):
-    def search(self, nums, target):
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         if not nums:
             return -1
-
-        low, high = 0, len(nums) - 1
-
-        while low <= high:
-            mid = (low + high) / 2
-            if target == nums[mid]:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
                 return mid
-
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    high = mid - 1
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    r = mid - 1
                 else:
-                    low = mid + 1
+                    l = mid + 1
             else:
-                if nums[mid] <= target <= nums[high]:
-                    low = mid + 1
+                if nums[mid] < target <= nums[len(nums) - 1]:
+                    l = mid + 1
                 else:
-                    high = mid - 1
-
+                    r = mid - 1
         return -1
+
 
 nums = [4,5,6,7,0,1,2]
 target = 0 
