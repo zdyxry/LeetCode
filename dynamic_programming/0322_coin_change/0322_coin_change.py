25b722318abf5d83fbb34fdb4c67d88fb8c9a247
zdyxry <zdyxry@gmail.com>
2022-03-15 08:58:46 +0800

    update 0322

--- dynamic_programming/0322_coin_change/0322_coin_change.py
+++ dynamic_programming/0322_coin_change/0322_coin_change.py
@@ -1,15 +1,13 @@
 
 class Solution(object):
     def coinChange(self, coins, amount):
-        INF = 0x7fffffff
-        amounts = [INF] * (amount +1)
-        amounts[0] = 0
-        for i in xrange(amount+1):
-            if amounts[i] != INF:
-                for coin in coins:
-                    if i + coin <= amount:
-                        amounts[i + coin] = min(amounts[i + coin], amounts[i] +1)
-        return amounts[amount] if amounts[amount] != INF else -1
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1 
 
 
 coins = [1,2,5]
