281606f5133064a1b2eb0d08042735e931535b9d
zdyxry <zdyxry@gmail.com>
2022-03-30 18:11:30 +0800

    update 0015

--- array/0015_3_sum/0015_3_sum.py
+++ array/0015_3_sum/0015_3_sum.py
@@ -1,16 +1,16 @@
-class Solution(object):
-    def threeSum(self, nums):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         res = []
         nums.sort()
         length = len(nums)
-        for i in xrange(length - 2):
-            if nums[i] > 0:
-                break
+        for i in range(length - 2):
+            if nums[i] > 0 :
+                continue
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-        
-            l, r = i + 1, length - 1
-            while l < r:
+
+            l, r = i + 1, length -1
+            while l < r :
                 total = nums[i] + nums[l] + nums[r]
 
                 if total < 0 :
@@ -18,7 +18,7 @@ class Solution(object):
                 elif total > 0 :
                     r -= 1
                 else:
-                    res.append([nums[i], nums[l], nums[r]])
+                    res.append(nums[i], nums[l], nums[r])
                     while l < r and nums[l] == nums[l+1]:
                         l += 1
                     while l < r and nums[r] == nums[r-1]:
